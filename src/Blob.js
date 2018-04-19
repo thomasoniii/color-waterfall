2274c606460c80800583c39db77a47e2bf3b4cdd
Jim Thomason <jim@japanesewartuba.com>
2018-04-19 17:23:17 -0500

    changed the color ranges again

--- src/Blob.js
+++ src/Blob.js
@@ -19,13 +19,15 @@ export default class Blob extends Component {
     if (! this.state.kids) { return null };
 
     const slice = (this.props.end - this.props.start) / this.state.kids;
+    const sliceStart = slice * (this.state.kids > 1 ? .25 : 0);
+    const sliceWidth = slice * (this.state.kids > 1 ? .75 : 1);
     const kidWidth = this.props.width / this.state.kids;
 
     if (kidWidth < 1 || 2 * this.state.kids > this.props.width ) { return null }
 
     for (let i = 0; i < this.state.kids; i++) {
-      const start = this.props.start + i * slice;
-      const end = start + slice;
+      const start = this.props.start + sliceStart + i * sliceWidth;
+      const end = start + sliceWidth;
 
       ret.push(
         <div style={{float:'left'}} key={i}>
@@ -33,7 +35,7 @@ export default class Blob extends Component {
             start={start}
             end={end}
             saturation={100}
-            brightness = {Math.max(this.props.brightness - 10, 10)}
+            brightness = {Math.max(this.props.brightness - 5, 15)}
             width={kidWidth}
             height={this.props.height}
           />
@@ -46,11 +48,12 @@ export default class Blob extends Component {
   render() {
 
     const { start, end, saturation, brightness, width, height } = this.props;
+    const hue = start + (end - start) / 2;
 
     return (
       <div>
         <div onClick = { this.addKid }
-          style={{boxSizing : 'border-box', width : `${width}px`, height : `${height}px`, backgroundColor : `hsl(${end}, ${saturation}%, ${brightness}%)`, border : '1px solid black'}}>
+          style={{boxSizing : 'border-box', width : `${width}px`, height : `${height}px`, backgroundColor : `hsl(${hue}, ${saturation}%, ${brightness}%)`, border : '1px solid black'}}>
         </div>
         { this.renderKids() }
       </div>
